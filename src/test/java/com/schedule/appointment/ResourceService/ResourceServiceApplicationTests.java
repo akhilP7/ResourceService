052c670ffaff0c0351c403272938ed4e858fb85a
akhilP7 <p.akhilreddy1289@gmail.com>
2023-05-22 15:34:50 -0500

    Initial commit - resource service creation

---
+++ src/test/java/com/schedule/appointment/ResourceService/ResourceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schedule.appointment.ResourceService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
